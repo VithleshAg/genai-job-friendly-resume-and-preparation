7bf4c512cf519a1156724fe4c28f840587ff9874
Vithlesh Agrawal <vithlesh@Vithleshs-MacBook-Air.local>
2026-04-12 12:39:56 +0530

    created Login and Register page in vite-react project

---
+++ Frontend/src/features/auth/hooks/useAuth.js
@@ -0,0 +1,75 @@
+import { useContext } from "react"
+import { AuthContext } from "../context"
+import { login, register, logout, getMe } from "../services/authService"
+import { useEffect } from "react"
+
+export const useAuth = () => {
+    const { loading, setLoading, user, setUser, message, setMessage } = useContext(AuthContext)
+
+    const handleLogin = async ({ email, password }) => {
+        setLoading(true)
+        try {
+            const data = await login({ email, password })
+            setUser(data.user)
+            setMessage({ type: 'success', text: data.message || 'Login successful' })
+        } catch (error) {
+            setMessage({ type: 'error', text: error.response?.data?.message || 'Login failed' })
+            console.error("Login failed:", error)
+        } finally {
+            setLoading(false)
+        }
+    }
+
+    const handleRegister = async ({ name, email, password }) => {
+        setLoading(true)
+        try {
+            const data = await register({ name, email, password })
+            setUser(data.user)
+            setMessage({ type: 'success', text: data.message || 'Registration successful' })
+        } catch (error) {
+            setMessage({ type: 'error', text: error.response?.data?.message || 'Registration failed' })
+            console.error("Registration failed:", error)
+        } finally {
+            setLoading(false)
+        }
+    }
+
+    const handleLogout = async () => {
+        setLoading(true)
+        try {
+            const data = await logout()
+            setUser(null)
+            setMessage({ type: 'success', text: data.message || 'Logout successful' })
+        } catch (error) {
+            setMessage({ type: 'error', text: error.response?.data?.message || 'Logout failed' })
+            console.error("Logout failed:", error)
+        } finally {
+            setLoading(false)
+        }
+    }
+
+    useEffect(() => {
+        const getAndSetUser = async () => {
+            try {
+                const data = await getMe()
+                setUser(data.user)
+            } catch (error) {
+                console.error("Failed to fetch user:", error)
+            } finally {
+                setLoading(false)
+            }
+        }
+        getAndSetUser()
+    }, [])
+
+    useEffect(() => {
+        if (message.text) {
+            const timer = setTimeout(() => {
+                setMessage({ type: '', text: '' })
+            }, 3000)
+            return () => clearTimeout(timer)
+        }
+    }, [message])
+
+    return { loading, handleLogin, handleRegister, handleLogout, user, message, setMessage }
+}
